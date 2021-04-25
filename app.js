bcd2d58fd2b38ea3aa6267adb2b51229cc4c40d4
aliabusharifeh <aliabushareefah@gmail.com>
2021-04-25 14:30:37 +0300

    addinfo

---
+++ app.js
@@ -0,0 +1,9 @@
+let yourname =prompt('add your name please');
+console.log(yourname);
+let yourage=prompt('add your age please');
+console.log(yourage);
+let yourmajor=prompt('what is your major ');
+console.log(yourmajor);
+ let yourcolor=prompt('what is your fav color?');
+ console.log(yourcolor);
+alert(' hello my name is  '+yourname+' i am '+yourage+'  my major is '+yourmajor+'  and my fav color is '+yourcolor );
